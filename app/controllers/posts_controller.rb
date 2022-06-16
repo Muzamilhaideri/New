ffd4f8acf0c321962b5e983f3ae364a16217f8a8
Muzamil Hussain <haiderimuzamil@gmail.com>
2022-06-16 22:53:03 +0500

    index and show method added to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,18 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+  def index
+    @post = Posts.all
+  end
+  def show
+    @post = Post.find(params[:id])
+  end
+
+
+
+  private
+  def post_params
+    params.require(:post).permit(:name, :body)
+  end
+
 end
