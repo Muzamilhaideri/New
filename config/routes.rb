26b025747793e6ef79729541fcc57109f00ecf9b
MuzamilHussainHaideri <haiderimuzamil@gmail.com>
2022-06-16 11:18:08 +0500

    grouping of route with namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :articles
+  end
   resources :articles
   root "articles#index"
   get "article/id", to: "articles#show"
+
 end
