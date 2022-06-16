4056a1804266767a85df2012fbacfb7c7ccf19fc
Muzamil Hussain <haiderimuzamil@gmail.com>
2022-06-16 23:02:51 +0500

    update and delete method added to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,9 +18,22 @@ class PostsController < ApplicationController
     else
       render'new'
     end
-
   end
 
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @posts
+    else
+      render'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+  end
 
 
   private
