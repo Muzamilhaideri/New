905433f4027d1d5522708ff3646bd8f164ad2d3a
Muzamil Hussain <muzamil.hussain@xcorebit.com>
2022-06-17 15:02:40 +0500

    edit view added to post + error fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,7 +24,7 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
 
     if @post.update(post_params)
-      redirect_to @posts
+      redirect_to @post
     else
       render'edit'
     end
