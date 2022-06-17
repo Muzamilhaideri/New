847469cf914894d328718f0b91e70aada8ea5a11
Muzamil Hussain <haiderimuzamil@gmail.com>
2022-06-17 09:50:37 +0500

    posts model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_16_054839) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_16_180827) do
   create_table "articles", force: :cascade do |t|
     t.string "Title"
     t.text "Text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "name"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
