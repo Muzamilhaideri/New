b010a175342b5686077280c8ddd88ad903fb8d96
Muzamil Hussain <muzamil.hussain@xcorebit.com>
2022-06-20 10:33:35 +0500

    uniqueness validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
   validates :Title, presence: true, format: { with: /\A[a-zA-Z]+\z/,
-            message: "only allows letters" },length: {minimum: 2}
-  validates :Text, length: {maximum: 500}, absence: true 
+            message: "only allows letters" },length: {minimum: 2}, uniqueness: true 
+  validates :Text, length: {maximum: 500}, absence: true
 end
