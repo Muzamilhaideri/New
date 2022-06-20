8f058404c73c91c195937a54afcec0d36637671f
Muzamil Hussain <muzamil.hussain@xcorebit.com>
2022-06-20 10:29:15 +0500

    format validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,4 @@
 class Article < ApplicationRecord
-  validates :Title, presence: true
+  validates :Title, presence: true, format: { with: /\A[a-zA-Z]+\z/,
+            message: "only allows letters" }
 end
