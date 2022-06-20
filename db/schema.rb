75196c7ba13d03c48d2f59fcfcfd9a7f3e77b7b6
Muzamil Hussain <muzamil.hussain@xcorebit.com>
2022-06-20 14:01:00 +0500

    belongs_to and has_many association added to users and stories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_16_180827) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_20_061514) do
   create_table "articles", force: :cascade do |t|
     t.string "Title"
     t.text "Text"
@@ -25,4 +25,21 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_16_180827) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "stories", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_stories_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "stories", "users"
 end
