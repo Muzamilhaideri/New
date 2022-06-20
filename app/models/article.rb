936ea10c1eca451810c60e2c2bbd7dbde4eedfd4
Muzamil Hussain <muzamil.hussain@xcorebit.com>
2022-06-20 10:40:03 +0500

    on validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
   validates :Title, format: { with: /\A[a-zA-Z]+\z/,
-            message: "only allows letters" },length: {minimum: 2}, uniqueness: true,  presence: { message: "must be given please" }
+            message: "only allows letters" },length: {minimum: 2}, uniqueness: true,  presence: { message: "must be given please" }, on: :create
   validates :Text, length: {maximum: 500}, absence: true
 end
