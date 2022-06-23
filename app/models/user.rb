ebb48b60b476debecd8e5bf31fc4bfee57057f9d
Muzamil Hussain <muzamil.hussain@xcorebit.com>
2022-06-23 16:30:00 +0500

    Added callback

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ApplicationRecord
   has_and_belongs_to_many :roles
   has_many :pictures, as: :imageable
   has_one :category, through: :profile
+
+  before_validation :normalize_name, on: :create
+
+  private
+  def normalize_name
+    self.name = name.downcase
+  end
+
 end
